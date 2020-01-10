4ae67a7875826c00cdbea048ceccad14df5120a9
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-10 12:59:25 +0000

    Prototyping resource url generation.

--- etv-media-scraper.rb
+++ etv-media-scraper.rb
@@ -1,10 +1,19 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'json'
+require 'net/https'
+require 'time'
+require 'uri'
 
 require_relative 'lib/etv_media_scraper_config'
 
 class EtvMediaScraper
+  @@etv_api_url = 'https://etv.err.ee/api/tv/getCategoryPastShows?category='
+  @@etv2_api_url = 'https://etv2.err.ee/api/tv/getCategoryPastShows?category='
+  @@api_params_ts_string = '&periodStart=0&periodEnd=' + Time.now.to_i.to_s
+  @@api_params_string = '&fullData=1'
+
   def initialize
     @tmp_path = File.join(File.dirname(__FILE__), 'tmp')
     @loot_path = File.join(File.dirname(__FILE__), 'loot')
@@ -16,15 +25,27 @@ class EtvMediaScraper
     @entities = @config.entities
   end
 
-  def add_task
+  def process_entity(entity)
+    url = build_resource_url(entity)
+    puts url
   end
 
-  def process_task
+  def build_resource_url(entity)
+    if entity.etv2
+      url = @@etv2_api_url
+    else
+      url = @@etv_api_url
+    end
+
+    url.concat(entity.category.to_s)
+    url.concat(@@api_params_string)
+
+    url
   end
 
   def run
-    @entities.each do |num|
-      puts num.category
+    @entities.each do |entity|
+      process_entity(entity)
     end
   end
 end
