8eecbdb1d189b2da59d6c15143a321993f1f18e8
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-15 13:08:22 +0000

    Added skip property to lib/etv_media_scraper_entity.rb

--- lib/etv_media_scraper_entity.rb
+++ lib/etv_media_scraper_entity.rb
@@ -1,18 +1,20 @@
 class EtvMediaScraperEntity
   attr_accessor :name
-  attr_reader :category, :etv2, :referer
+  attr_reader :skip, :category, :etv2, :referer
 
   def initialize(hash)
+    @skip = false
     @name = nil
     @etv2 = false
     @referer = nil
 
+    @skip = hash['skip'] if hash.key?('skip')
     @category = hash['category'].to_i if hash.key?('category')
     @name = hash['name'] if hash.key?('name')
     @etv2 = hash['etv2'] if hash.key?('etv2')
   end
 
   def valid?
-    @category && @category.is_a?(Integer)
+    !@skip && @category && @category.is_a?(Integer)
   end
 end
