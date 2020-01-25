5da190466c6861df4ab7cd06d879c83ab47e3083
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-25 03:24:07 +0000

    Added basic progress indication for downloader class.

--- lib/etv_media_scraper_downloader.rb
+++ lib/etv_media_scraper_downloader.rb
@@ -1,18 +1,19 @@
 require 'fileutils'
 require 'net/https'
+require 'progressbar'
 require 'uri'
 
 class EtvMediaScraperDownloader
+  @@skip_path = File.join(File.dirname(__FILE__), '../skip')
+
   def initialize(source_url = nil, destination_path = nil)
     @source_url = source_url
     @destination_path = destination_path
-
-    @skip_path = File.join(File.dirname(__FILE__), '../skip')
   end
 
   def run
     basename = File.basename(@source_url)
-    skip_file = File.join(@skip_path, basename)
+    skip_file = File.join(@@skip_path, basename)
     destination_file = File.join(@destination_path, basename)
 
     if File.file?(skip_file)
@@ -27,12 +28,21 @@ class EtvMediaScraperDownloader
       response = Net::HTTP.start(uri.host, uri.port,
       :use_ssl => uri.scheme == 'https',
       :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-      request = Net::HTTP::Get.new(uri.request_uri)
+        request = Net::HTTP::Get.new(uri.request_uri)
+        progressbar = ProgressBar.create(
+          :format => "%a %b\u{15E7}%i %p%% %t",
+          :progress_mark => ' ',
+          :remainder_mark => "\u{FF65}")
 
         http.request(request) do |response|
+          if response.header['content-length']
+            progressbar.total = response.header['content-length'].to_i
+          end
+
           File.open(destination_file, 'wb') do |file|
             response.read_body do |chunk|
               file.write(chunk)
+              progressbar.progress += chunk.length
             end
           end
         end
