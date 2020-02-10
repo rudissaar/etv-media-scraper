74d8b2395390394468e1c9dafbabbee2eda217f7
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-02-10 21:38:06 +0000

    Improved EtvMediaScraperEpisode class.

--- lib/etv_media_scraper_episode.rb
+++ lib/etv_media_scraper_episode.rb
@@ -1,6 +1,15 @@
+require_relative 'etv_media_scraper_downloader'
+
 class EtvMediaScraperEpisode
   attr_accessor :url, :season, :number
 
-  def initialize()
+  @allowed_options = %w[url season number]
+
+  def initialize(options = {})
+    options.each do |option, value|
+      if @allowed_options.include?(option)
+        instance_variable_set("@#{options}") unless value.nil?
+      end
+    end
   end
 end
