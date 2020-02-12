ff7bfdf1d42fe8216e1acc3043dcc3f9cd93c30c
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-02-12 13:36:31 +0000

    Improved downloader class + added .rubocop.yml file.

--- lib/etv_media_scraper_downloader.rb
+++ lib/etv_media_scraper_downloader.rb
@@ -5,11 +5,14 @@ require 'uri'
 
 require_relative 'etv_media_scraper_config'
 
+# Class that handles downloading media files.
 class EtvMediaScraperDownloader
-  attr_accessor :url, :destination
+  attr_accessor :url, :destination, :verbose
 
   def initialize
-    allowed_options = %w[use_wget]
+    @verbose = true
+
+    allowed_options = %w[use_wget verbose]
     config = EtvMediaScraperConfig.new
 
     config.downloader_options.each do |option, value|
@@ -23,22 +26,30 @@ class EtvMediaScraperDownloader
     system("wget #{@url} -O #{@destination}")
   end
 
-  def run_native
+  def native_http_options
     uri = URI(@url)
+    options = { options: {} }
 
-    response = Net::HTTP.start(uri.host, uri.port,
-    :use_ssl => uri.scheme == 'https',
-    :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-      request = Net::HTTP::Get.new(uri.request_uri)
+    options[:uri] = uri
+    options[:options][:use_ssl] = uri.scheme == 'https'
+    options[:options][:verify_mode] = OpenSSL::SSL::VERIFY_NONE if options[:options][:use_ssl]
+
+    options
+  end
+
+  def run_native
+    options = native_http_options
+
+    Net::HTTP.start(options[:uri].host, options[:uri].port, options[:options]) do |http|
+      request = Net::HTTP::Get.new(options[:uri].request_uri)
       progressbar = ProgressBar.create(
-        :format => "%a %b\u{15E7}%i %p%% %t",
-        :progress_mark => ' ',
-        :remainder_mark => "\u{FF65}")
+        format: "%a %b\u{15E7}%i %p%% %t",
+        progress_mark: ' ',
+        remainder_mark: "\u{FF65}"
+      )
 
       http.request(request) do |response|
-        if response.header['content-length']
-          progressbar.total = response.header['content-length'].to_i
-        end
+        progressbar.total = response.header['content-length'].to_i if response.header['content-length']
 
         File.open(@destination, 'wb') do |file|
           response.read_body do |chunk|
@@ -51,7 +62,7 @@ class EtvMediaScraperDownloader
   end
 
   def run
-    puts('> Downloading: ' + @url)
+    puts('> Downloading: ' + @url) if @verbose
     @use_wget ? run_wget : run_native
   end
 end
