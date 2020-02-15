ef1241a06dec473dae9875ffbbf2ff0f2159e13e
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-02-15 21:30:38 +0000

    Improved Entity class (refactoring).

--- lib/etv_media_scraper_entity.rb
+++ lib/etv_media_scraper_entity.rb
@@ -5,12 +5,15 @@ class EtvMediaScraperEntity
   attr_accessor :name
   attr_reader :skip, :category, :etv2, :referer
 
-  def initialize(hash)
-    @hash = hash
-    @skip = hash['skip'] if hash.key?('skip')
-    @category = hash['category'].to_i if hash.key?('category')
-    @name = hash['name'] if hash.key?('name')
-    @etv2 = hash['etv2'] if hash.key?('etv2')
+  def initialize(options)
+    @options = options
+    allowed_options = %w[name skip category etv2 referer]
+
+    options.each do |option, value|
+      if allowed_options.include?(option)
+        instance_variable_set("@#{option}", value) unless value.nil?
+      end
+    end
 
     rules
   end
@@ -23,8 +26,8 @@ class EtvMediaScraperEntity
   end
 
   def source_rules
-    return unless @hash.key?('source_rules')
-    source_rules = @hash['source_rules']
+    return unless @options.key?('source_rules')
+    source_rules = @options['source_rules']
 
     if source_rules.key?('url_must_exclude')
       source_rules['url_must_exclude'].each do |string_to_exclude|
@@ -42,6 +45,6 @@ class EtvMediaScraperEntity
       return false if url[string_to_exclude]
     end
 
-    return true
+    true
   end
 end
