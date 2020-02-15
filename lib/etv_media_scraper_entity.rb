ef19a36851f85a50288c9c1b3b7ba3c04af77f96
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-02-15 16:58:15 +0000

    Minor improvements for Entity class.

--- lib/etv_media_scraper_entity.rb
+++ lib/etv_media_scraper_entity.rb
@@ -1,24 +1,34 @@
 require 'fileutils'
 
+# Class that handles task related data and logic.
 class EtvMediaScraperEntity
   attr_accessor :name
   attr_reader :skip, :category, :etv2, :referer
 
   def initialize(hash)
+    @hash = hash
     @skip = hash['skip'] if hash.key?('skip')
     @category = hash['category'].to_i if hash.key?('category')
     @name = hash['name'] if hash.key?('name')
     @etv2 = hash['etv2'] if hash.key?('etv2')
 
-    @rules = {}
-    @rules['url_must_exclude'] = []
+    rules
+  end
 
-    if hash.key?('source_rules')
-      source_rules = hash['source_rules']
-      if source_rules.key?('url_must_exclude')
-        source_rules['url_must_exclude'].each do |string_to_exclude|
-          @rules['url_must_exclude'].push(string_to_exclude)
-        end
+  def rules
+    @rules = {}
+    @rules[:url_must_exclude] = []
+
+    source_rules
+  end
+
+  def source_rules
+    return unless @hash.key?('source_rules')
+    source_rules = @hash['source_rules']
+
+    if source_rules.key?('url_must_exclude')
+      source_rules['url_must_exclude'].each do |string_to_exclude|
+        @rules[:url_must_exclude].push(string_to_exclude)
       end
     end
   end
@@ -28,7 +38,7 @@ class EtvMediaScraperEntity
   end
 
   def complient?(url)
-    @rules['url_must_exclude'].each do |string_to_exclude|
+    @rules[:url_must_exclude].each do |string_to_exclude|
       return false if url[string_to_exclude]
     end
 
