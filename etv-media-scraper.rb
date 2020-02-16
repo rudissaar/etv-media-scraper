f7c768d49229c3338c811f3a4caca1a38e221945
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-02-16 20:33:42 +0000

    Improved Scraper class.

--- etv-media-scraper.rb
+++ etv-media-scraper.rb
@@ -12,13 +12,14 @@ require_relative File.join('lib', 'etv_media_scraper_entity')
 require_relative File.join('lib', 'etv_media_scraper_episode')
 require_relative File.join('lib', 'etv_media_scraper_downloader')
 
+# Bootstrap class that connects everything together.
 class EtvMediaScraper
-  @@etv_api_url = 'https://etv.err.ee/api/tv/getCategoryPastShows?category='
-  @@etv2_api_url = 'https://etv2.err.ee/api/tv/getCategoryPastShows?category='
-  @@api_params_ts_string = '&periodStart=0&periodEnd=' + Time.now.to_i.to_s
-  @@api_params_string = '&fullData=1'
-
   def initialize
+    @etv_api_url = 'https://etv.err.ee/api/tv/getCategoryPastShows?category='
+    @etv2_api_url = 'https://etv2.err.ee/api/tv/getCategoryPastShows?category='
+    @api_params_ts_string = '&periodStart=0&periodEnd=' + Time.now.to_i.to_s
+    @api_params_string = '&fullData=1'
+
     @config = EtvMediaScraperConfig.new
     @entities = @config.entities
   end
@@ -29,39 +30,39 @@ class EtvMediaScraper
 
     fetch_resources(entity)
 
-    @episodes.each do |episode|
-      episode.download
-    end
+    @episodes.each(&:download)
   end
 
   def build_resource_url(entity)
-    if entity.etv2
-      url = @@etv2_api_url
-    else
-      url = @@etv_api_url
-    end
-
+    url = entity.etv2 ? @etv2_api_url : @etv_api_url
     url.concat(entity.category.to_s)
-    url.concat(@@api_params_string)
+    url.concat(@api_params_string)
 
-    return url
+    url
+  end
+
+  def resources_http_options
+    uri = URI(@resource_url)
+    options = { options: {} }
+
+    options[:uri] = uri
+    options[:options][:use_ssl] = uri.scheme == 'https'
+    options[:options][:verify_mode] = OpenSSL::SSL::VERIFY_NONE if options[:options][:use_ssl]
+
+    options
   end
 
   def fetch_resources(entity)
-    uri = URI(@resource_url)
-    
-    Net::HTTP.start(uri.host, uri.port,
-      :use_ssl => uri.scheme == 'https',
-      :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-      request = Net::HTTP::Get.new(uri.request_uri)
+    options = resources_http_options
+
+    Net::HTTP.start(options[:uri].host, options[:uri].port, options[:options]) do |http|
+      request = Net::HTTP::Get.new(options[:uri].request_uri)
 
       response = http.request(request)
       json = JSON.parse(response.body)
 
       json['data'].each do |obj|
-        unless entity.name
-          entity.name = obj['primaryCategory']['name']
-        end
+        entity.name = obj['primaryCategory']['name'] unless entity.name
 
         episode = EtvMediaScraperEpisode.new
 
